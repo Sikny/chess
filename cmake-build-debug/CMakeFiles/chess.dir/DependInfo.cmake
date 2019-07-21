88b38b540f7a851f77da19eb626fd05a852efa56
sikny <pamartqu@gmail.com>
2019-07-21 22:14:45 +0200

    CMake project conversion

---
+++ cmake-build-debug/CMakeFiles/chess.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/pamar/Documents/MesDocuments/Projets/chess/main.cpp" "C:/Users/pamar/Documents/MesDocuments/Projets/chess/cmake-build-debug/CMakeFiles/chess.dir/main.cpp.obj"
+  "C:/Users/pamar/Documents/MesDocuments/Projets/chess/src/bishop.cpp" "C:/Users/pamar/Documents/MesDocuments/Projets/chess/cmake-build-debug/CMakeFiles/chess.dir/src/bishop.cpp.obj"
+  "C:/Users/pamar/Documents/MesDocuments/Projets/chess/src/case.cpp" "C:/Users/pamar/Documents/MesDocuments/Projets/chess/cmake-build-debug/CMakeFiles/chess.dir/src/case.cpp.obj"
+  "C:/Users/pamar/Documents/MesDocuments/Projets/chess/src/game.cpp" "C:/Users/pamar/Documents/MesDocuments/Projets/chess/cmake-build-debug/CMakeFiles/chess.dir/src/game.cpp.obj"
+  "C:/Users/pamar/Documents/MesDocuments/Projets/chess/src/king.cpp" "C:/Users/pamar/Documents/MesDocuments/Projets/chess/cmake-build-debug/CMakeFiles/chess.dir/src/king.cpp.obj"
+  "C:/Users/pamar/Documents/MesDocuments/Projets/chess/src/knight.cpp" "C:/Users/pamar/Documents/MesDocuments/Projets/chess/cmake-build-debug/CMakeFiles/chess.dir/src/knight.cpp.obj"
+  "C:/Users/pamar/Documents/MesDocuments/Projets/chess/src/pawn.cpp" "C:/Users/pamar/Documents/MesDocuments/Projets/chess/cmake-build-debug/CMakeFiles/chess.dir/src/pawn.cpp.obj"
+  "C:/Users/pamar/Documents/MesDocuments/Projets/chess/src/piece.cpp" "C:/Users/pamar/Documents/MesDocuments/Projets/chess/cmake-build-debug/CMakeFiles/chess.dir/src/piece.cpp.obj"
+  "C:/Users/pamar/Documents/MesDocuments/Projets/chess/src/queen.cpp" "C:/Users/pamar/Documents/MesDocuments/Projets/chess/cmake-build-debug/CMakeFiles/chess.dir/src/queen.cpp.obj"
+  "C:/Users/pamar/Documents/MesDocuments/Projets/chess/src/rook.cpp" "C:/Users/pamar/Documents/MesDocuments/Projets/chess/cmake-build-debug/CMakeFiles/chess.dir/src/rook.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "C:/Users/pamar/Documents/Libs/SFML-2.5.1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/chess.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chess.dir/src/bishop.cpp.obj"
+  "CMakeFiles/chess.dir/src/case.cpp.obj"
+  "CMakeFiles/chess.dir/src/game.cpp.obj"
+  "CMakeFiles/chess.dir/src/king.cpp.obj"
+  "CMakeFiles/chess.dir/src/knight.cpp.obj"
+  "CMakeFiles/chess.dir/src/pawn.cpp.obj"
+  "CMakeFiles/chess.dir/src/piece.cpp.obj"
+  "CMakeFiles/chess.dir/src/queen.cpp.obj"
+  "CMakeFiles/chess.dir/src/rook.cpp.obj"
+  "CMakeFiles/chess.dir/main.cpp.obj"
+  "chess.pdb"
+  "chess.exe"
+  "chess.exe.manifest"
+  "libchess.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
