0e3cbbd0f6f758faa67f2d7c77cce4cd464018e3
sikny <pamartqu@gmail.com>
2019-07-23 00:31:28 +0200

    rook basic moving

--- src/rook.cpp
+++ src/rook.cpp
@@ -3,5 +3,9 @@
 Rook::Rook(sf::Color color) : Piece(color){}
 
 bool Rook::canBeMoved(const std::string &pos) {
-    return true;
+    int colFrom = position.at(1) - '0';
+    int colDest = pos.at(1) - '0';
+    char rowFrom = position.at(0);
+    char rowDest = pos.at(0);
+    return colFrom == colDest || rowFrom == rowDest;
 }
