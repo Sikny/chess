439f3442e79ce017cd4c368f97e370bc43791f0b
sikny <pamartqu@gmail.com>
2019-07-22 21:33:05 +0200

    pawn basic moving

--- src/pawn.cpp
+++ src/pawn.cpp
@@ -3,5 +3,11 @@
 Pawn::Pawn(sf::Color color) : Piece(color){}
 
 bool Pawn::canBeMoved(const std::string &pos) {
-    return true;
+    int colFrom = position.at(1) - '0';
+    int colDest = pos.at(1) - '0';
+    if(color == sf::Color::White && (colDest == colFrom+1 || (colFrom == 2 && colDest == colFrom+2)))
+        return true;
+    else if(color == sf::Color::Black && (colDest == colFrom-1 || (colFrom == 7 && colDest == colFrom-2)))
+        return true;
+    return false;
 }
