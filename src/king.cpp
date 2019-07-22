de8438ba63300292397304511e576042c097e62e
sikny <pamartqu@gmail.com>
2019-07-23 00:28:29 +0200

    king basic moving

--- src/king.cpp
+++ src/king.cpp
@@ -3,5 +3,10 @@
 King::King(sf::Color color) : Piece(color){}
 
 bool King::canBeMoved(const std::string &pos) {
-    return true;
+    int colFrom = position.at(1) - '0';
+    int colDest = pos.at(1) - '0';
+    char rowFrom = position.at(0);
+    char rowDest = pos.at(0);
+    return (colFrom == colDest + 1 || colFrom == colDest - 1 || colFrom == colDest)
+           && (rowFrom == rowDest + 1 || rowFrom == rowDest - 1 || rowFrom == rowDest);
 }
