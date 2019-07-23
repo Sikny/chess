119c82e33839a9c7bb3ab3c8e23f0799fa21cea6
sikny <pamartqu@gmail.com>
2019-07-23 15:14:12 +0200

    queen basic moving

--- src/queen.cpp
+++ src/queen.cpp
@@ -3,5 +3,11 @@
 Queen::Queen(sf::Color color) : Piece(color) {}
 
 bool Queen::canBeMoved(const std::string &pos) {
-    return true;
+    int colFrom = position.at(1) - '0';
+    int colDest = pos.at(1) - '0';
+    char rowFrom = position.at(0);
+    char rowDest = pos.at(0);
+    return (colFrom == colDest || rowFrom == rowDest)  // rook
+        || (colDest-colFrom == rowDest-rowFrom || colDest-colFrom == -(rowDest-rowFrom));    // bishop
+
 }
