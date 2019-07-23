6454b9f6bed5099f3b8a5470135c7f9383defd72
sikny <pamartqu@gmail.com>
2019-07-23 15:19:57 +0200

    knight basic moving

--- src/knight.cpp
+++ src/knight.cpp
@@ -3,5 +3,10 @@
 Knight::Knight(sf::Color color) : Piece(color) {}
 
 bool Knight::canBeMoved(const std::string &pos) {
-    return true;
+    int colFrom = position.at(1) - '0';
+    int colDest = pos.at(1) - '0';
+    char rowFrom = position.at(0);
+    char rowDest = pos.at(0);
+    return ((colDest == colFrom-1 || colDest == colFrom+1) && (rowDest == rowFrom+2 || rowDest == rowFrom-2))
+        || ((colDest == colFrom-2 || colDest == colFrom+2) && (rowDest == rowFrom+1 || rowDest == rowFrom-1));
 }
